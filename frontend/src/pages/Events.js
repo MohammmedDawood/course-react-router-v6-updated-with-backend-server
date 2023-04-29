d5f2cf19b1820b259c97448ea56c7a05a2aadf10
Mohammed Dawood <mahmedtony73@gmail.com>
2023-04-29 09:52:32 +0300

    Using Data From A Loader In The Route Component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,48 +1,12 @@
-import { Link } from "react-router-dom";
-
-const DUMMY_EVENTS = [
-  {
-    id: "e1",
-    title: "Programming for everyone",
-    description: "In this course, you will learn how to program in Python!",
-    location: "Somewhere in the world",
-    date: "2021-05-12",
-    image: "images/coding-event.jpg",
-    isFeatured: false,
-  },
-  {
-    id: "e2",
-    title: "Networking for introverts",
-    description:
-      "You probably need this course if you already struggle with talking to people.",
-    location: "Somewhere in the world",
-    date: "2021-05-30",
-    image: "images/introvert-event.jpg",
-    isFeatured: true,
-  },
-  {
-    id: "e3",
-    title: "Networking for extroverts",
-    description:
-      "You probably need this course if you already struggle with talking to people.",
-    location: "Somewhere in the world",
-    date: "2021-05-30",
-    image: "images/introvert-event.jpg",
-    isFeatured: true,
-  },
-];
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
+  const fetchedEvents = useLoaderData();
+
   return (
     <>
-      <h1>EventsPage</h1>
-      <ul>
-        {DUMMY_EVENTS.map((event) => (
-          <li key={event.id}>
-            <Link to={`${event.id}`}>{event.title}</Link>
-          </li>
-        ))}
-      </ul>
+      <EventsList events={fetchedEvents} />
     </>
   );
 }
