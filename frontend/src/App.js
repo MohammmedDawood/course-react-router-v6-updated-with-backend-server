bb6cca515a6555f457af6d3d83ddc6877567aedb
Mohammed Dawood <mahmedtony73@gmail.com>
2023-04-29 15:30:53 +0300

    Where Should loader() Code Be Stored?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,37 +1,7 @@
-// Challenge / Exercise
-
-// 1. Add five new (dummy) page components (content can be simple <h1> elements)
-//    - HomePage
-//    - EventsPage
-//    - EventDetailPage
-//    - NewEventPage
-//    - EditEventPage
-//    Done
-// 2. Add routing & route definitions for these five pages
-//    - / => HomePage
-//    - /events => EventsPage
-//    - /events/<some-id> => EventDetailPage
-//    - /events/new => NewEventPage
-//    - /events/<some-id>/edit => EditEventPage
-//    Done
-// 3. Add a root layout that adds the <MainNavigation> component above all page components
-//    Done
-// 4. Add properly working links to the MainNavigation
-//    Done
-// 5. Ensure that the links in MainNavigation receive an "active" class when active
-//    Done
-// 6. Output a list of dummy events to the EventsPage
-//    Every list item should include a link to the respective EventDetailPage
-//    Done
-// 7. Output the ID of the selected event on the EventDetailPage
-//    Done
-// BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
-//    Done
-
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import HomePage from "./pages/Home";
-import EventsPage from "./pages/Events";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventDetailPage from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
@@ -56,15 +26,7 @@ const router = createBrowserRouter([
             index: true,
             // path: "events",
             element: <EventsPage />,
-            loader: async () => {
-              const response = await fetch("http://localhost:8080/events");
-
-              if (!response.ok) {
-              } else {
-                const resData = await response.json();
-                return resData.events;
-              }
-            },
+            loader: eventsLoader,
           },
           {
             path: ":eventId",
